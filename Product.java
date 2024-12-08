2ee0811a2d08480bdb03537182b96a443df4f022
Anil <an.kumar3407@gmail.com>
2024-12-08 18:02:26 +0530

    Product java file modified

--- Product.java
+++ Product.java
@@ -1,5 +1,9 @@
 class Product{
     public static void main(String []args){
         System.out.println("Hellow World");
+        int j=10;
+        System.out.println(j);
+        int k=20;
+        
     }
 }
