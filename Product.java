ec8faf8896aa835ed11a37d6db9b3bf054220a4a
Anil <an.kumar3407@gmail.com>
2024-12-08 18:05:50 +0530

    addition method creatd

--- Product.java
+++ Product.java
@@ -4,6 +4,10 @@ class Product{
         int j=10;
         System.out.println(j);
         int k=20;
-        
+        public static void addition(){
+            var result=j+k;
+            System.out.println(result);
+        }
+
     }
 }
